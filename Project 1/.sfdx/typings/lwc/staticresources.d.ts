3f83084fd52726f146ffba54e781d49a8d0cc72a
Christopher Brefo <christopherbrefo@yahoo.com>
2020-08-05 13:03:54 -0400

    Retrieved PageLayout and CustomField

--- Project 1/.sfdx/typings/lwc/staticresources.d.ts
+++ Project 1/.sfdx/typings/lwc/staticresources.d.ts
@@ -2,3 +2,7 @@ declare module "@salesforce/resourceUrl/SiteSamples" {
     var SiteSamples: string;
     export default SiteSamples;
 }
+declare module "@salesforce/resourceUrl/customCSS" {
+    var customCSS: string;
+    export default customCSS;
+}
